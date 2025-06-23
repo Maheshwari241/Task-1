335c15dc20374131e757f8c7f8d96471b33c6c16
Maheshwari Murugavel <159159372+Maheshwari241@users.noreply.github.com>
2025-06-23 14:02:39 +0530

    Add files via upload

---
+++ schme.sql
@@ -0,0 +1,45 @@
+
+CREATE DATABASE IF NOT EXISTS LibraryDB;
+USE LibraryDB;
+
+DROP TABLE IF EXISTS Borrow;
+DROP TABLE IF EXISTS BookAuthors;
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Authors;
+DROP TABLE IF EXISTS Members;
+
+CREATE TABLE Authors (
+    AuthorID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Bio TEXT
+);
+CREATE TABLE Books (
+    BookID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(150) NOT NULL,
+    ISBN VARCHAR(20) UNIQUE,
+    Publisher VARCHAR(100),
+    Year YEAR
+);
+CREATE TABLE BookAuthors (
+    BookID INT,
+    AuthorID INT,
+    PRIMARY KEY (BookID, AuthorID),
+    FOREIGN KEY (BookID) REFERENCES Books(BookID),
+    FOREIGN KEY (AuthorID) REFERENCES Authors(AuthorID)
+);
+CREATE TABLE Members (
+    MemberID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100) UNIQUE,
+    Phone VARCHAR(15),
+    JoinDate DATE
+);
+CREATE TABLE Borrow (
+    BorrowID INT AUTO_INCREMENT PRIMARY KEY,
+    MemberID INT,
+    BookID INT,
+    BorrowDate DATE,
+    ReturnDate DATE,
+    FOREIGN KEY (MemberID) REFERENCES Members(MemberID),
+    FOREIGN KEY (BookID) REFERENCES Books(BookID)
+);
